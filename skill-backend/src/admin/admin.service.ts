76666ae8d52617e260159bf00db5cdfc94147f46
Dhaarun MK <dhaarunmk@kyyba.com>
2026-07-14 16:17:22 +0530

    added requireTLS as true

--- skill-backend/src/admin/admin.service.ts
+++ skill-backend/src/admin/admin.service.ts
@@ -74,11 +74,12 @@ export class AdminService {
     if (!user) throw new NotFoundException('Employee not found');
 
     const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
+      host: 'smtp.gmail.com',
+      port: 587,
       secure: false,
+      requireTLS: true,
       auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
-      // tls: { rejectUnauthorized: false },
+      tls: { rejectUnauthorized: false },
     });
 
     try {
--- skill-backend/src/auth/auth.service.ts
+++ skill-backend/src/auth/auth.service.ts
@@ -35,11 +35,12 @@ export class AuthService {
 
   private getTransporter() {
     return nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
+      host: 'smtp.gmail.com',
+      port: 587,
       secure: false,
+      requireTLS: true,
       auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
-      // tls: { rejectUnauthorized: false },
+      tls: { rejectUnauthorized: false },
     });
   }
 
